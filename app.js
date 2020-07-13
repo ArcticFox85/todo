dc727d70afb5339900044312be1b6d2c2f85ed0a
ArcticFox85 <60521197+ArcticFox85@users.noreply.github.com>
2020-07-13 21:18:34 +0300

    completed todo

--- app.js
+++ app.js
@@ -1,31 +1,47 @@
- const express =require("express");
+ const express = require("express");
  const bodyParser = require("body-parser");
 
  const app = express();
-
+ var items = ["Buy Food",
+     "Cook",
+     "Eat Food"
+ ];
  app.set('view engine', 'ejs');
 
- app.get("/", function(req,res){
+ app.use(bodyParser.urlencoded({
+     extended: true
+ }));
 
-    var today = new Date();
-var options = {
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-} ;
+app.use(express.static("public"));
+ app.get("/", function (req, res) {
 
-var day =today.toLocaleString('en-US', options);
+     var today = new Date();
+     var options = {
+         weekday: "long",
+         day: "numeric",
+         month: "long"
+     };
 
-    res.render("list", {
-        kindOfDay: day
-    });
+     var day = today.toLocaleString('en-US', options);
 
+     res.render("list", {
+         kindOfDay: day,
+         newListItems: items
+     });
+
+ });
+
+ app.post("/", function (req, res) {
+
+     var item = req.body.newItem;
+     items.push(item);
+     res.redirect("/");
  });
 
 
 
- app.listen(3000, function(){
-console.log("Server started on port 3000");
+ app.listen(3000, function () {
+     console.log("Server started on port 3000");
 
 
- }); 
+ });
